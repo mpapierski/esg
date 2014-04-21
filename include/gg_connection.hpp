fd4d40af0670af2abc5fd8f2aa4c262b0ce78be6
Michał Papierski <michal@papierski.net>
2014-04-21 19:31:41 +0200

    Read gg header packet.

--- include/gg_connection.hpp
+++ include/gg_connection.hpp
@@ -2,9 +2,14 @@
 #define ESG_GG_CONNECTION_HPP_INCLUDED_
 
 #include <boost/asio.hpp>
+#include <boost/enable_shared_from_this.hpp>
 #include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
+#include <boost/bind.hpp>
+#include "libgadu.h"
 
 class gg_connection
+	: public boost::enable_shared_from_this<gg_connection>
 {
 public:
 	typedef boost::shared_ptr<gg_connection> pointer;
@@ -17,6 +22,8 @@ public:
 		return socket_;
 	}
 	void start();
+	void handle_read_gg_header(const boost::system::error_code & errorr,
+		std::size_t bytes_transferred, boost::shared_ptr<struct gg_header> gg_header);
 private:
 	gg_connection(boost::asio::io_service & io_service);
 	boost::asio::io_service & io_service_;
--- src/gg_connection.cpp
+++ src/gg_connection.cpp
@@ -9,5 +9,23 @@ gg_connection::gg_connection(boost::asio::io_service & io_service)
 
 void gg_connection::start()
 {
-	
+	// Create gg_header and read data from client directly into it
+	boost::shared_ptr<struct gg_header> header = boost::make_shared<struct gg_header>();
+	auto bufs = boost::asio::buffer(
+		reinterpret_cast<char *>(header.get()),
+		sizeof(struct gg_header));
+	// Begin asynchronous operation
+	boost::asio::async_read(socket_, bufs,
+		boost::bind(&gg_connection::handle_read_gg_header, shared_from_this(),
+			boost::asio::placeholders::error,
+			boost::asio::placeholders::bytes_transferred,
+			header));
+}
+
+void gg_connection::handle_read_gg_header(const boost::system::error_code & error,
+	std::size_t bytes_transferred, boost::shared_ptr<struct gg_header> gg_header)
+{
+	if (!error)
+	{
+	}
 }
